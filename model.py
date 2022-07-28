d5ff06c48461cfdb5df9b4600d705c6c7930a1b3
samrich76 <samrich76@hotmail.com>
2022-07-27 22:21:24 -0400

    added ml related code

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("adding all ml related code" )
+
+def model1():
+    print("adding mode ml model" )
+
+def model2():
+    print("adding mode ml model" )
+
+def model3():
+    print("adding mode ml model" )        
